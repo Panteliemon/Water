e477f5959ff0be6190e805177661c9e49f1ce8a5
Panteliemon <bondareff89@gmail.com>
2024-06-19 22:48:16 +0300

    Certificates for production environment

--- WaterServer/WaterServer/Program.cs
+++ WaterServer/WaterServer/Program.cs
@@ -1,20 +1,44 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-namespace WaterServer
+namespace WaterServer;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        string secretsFolderPath = builder.Configuration[
+            builder.Environment.IsDevelopment() ? "App:SecretsPath:Home" : "App:SecretsPath:EC2"
+        ];
+
+        if (!builder.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            string certPath = Path.Combine(secretsFolderPath, builder.Configuration["App:CertBaseName"] + ".crt");
+            string keyPath = Path.Combine(secretsFolderPath, builder.Configuration["App:CertBaseName"] + ".key");
+            X509Certificate2 certificate = X509Certificate2.CreateFromPemFile(certPath, keyPath);
 
-            app.MapGet("/", () => "Hello World! 111");
-
-            app.Run();
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.Limits.MaxRequestBodySize = 1 << 20;
+                options.Listen(IPAddress.Any, 443, listenOptions =>
+                {
+                    listenOptions.UseHttps(certificate);
+                });
+            });
         }
+
+        WebApplication app = builder.Build();
+
+        app.MapGet("/", () => "Hello World! 111");
+
+        app.Run();
     }
 }
