230248769f063255ac9580405b66a7ed0b67c07b
Panteliemon <bondareff89@gmail.com>
2025-05-15 10:25:32 +0300

    Plant icons. Last activity time and counts per liter.

---
+++ WaterServer/WaterServer/Utils/DateUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WaterServer.Utils;
+
+public static class DateUtils
+{
+    private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+    /// <summary>
+    /// For UTC dates only!
+    /// </summary>
+    public static long UtcToJsMilliseconds(this DateTime dateTime)
+    {
+        return (long)Math.Round(dateTime.Subtract(Epoch).TotalMilliseconds);
+    }
+}
