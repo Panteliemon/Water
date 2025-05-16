6ec134895f19d6d1a07cea5c5a84de9907deeaf2
Panteliemon <bondareff89@gmail.com>
2025-05-16 13:21:01 +0300

    WORKED!!! Tidy things up

--- WaterServer/WaterServer/Program.cs
+++ WaterServer/WaterServer/Program.cs
@@ -25,13 +25,30 @@ public class Program
 
         if (!builder.Environment.IsDevelopment())
         {
-            string certPath = Path.Combine(secretsFolderPath, builder.Configuration["App:CertBaseName"] + ".crt");
-            string keyPath = Path.Combine(secretsFolderPath, builder.Configuration["App:CertBaseName"] + ".key");
-            string pfxPath = Path.Combine(secretsFolderPath, builder.Configuration["App:CertBaseName"] + ".pfx");
-            string passwordPath = Path.Combine(secretsFolderPath, "pass.txt");
-            string password = File.ReadAllText(passwordPath).Trim();
-            X509Certificate2 certificate = //X509Certificate2.CreateFromPemFile(certPath, keyPath);
-                new X509Certificate2(pfxPath, "");
+            // Dotnet doesn't load CRT/KEY PEM certificates if they were created
+            // through certificate signing request. It only parses self-signed certificates
+            // created in one step. So it obviously doesn't work with CA-signed certs bought for money.
+            // Thankfully, works with PFX format. Here in code both approaches are left.
+            X509Certificate2 certificate = null;
+            try
+            {
+                // The way I prefer
+                string certPath = Path.Combine(secretsFolderPath, builder.Configuration["App:CertBaseName"] + ".crt");
+                string keyPath = Path.Combine(secretsFolderPath, builder.Configuration["App:CertBaseName"] + ".key");
+                certificate = X509Certificate2.CreateFromPemFile(certPath, keyPath);
+            }
+            catch (Exception ex)
+            {
+                // PFX way
+                ConsoleColor prevColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("[x] ");
+                Console.ForegroundColor = prevColor;
+                Console.WriteLine("crt/key certificate pair - .net REFUSED to load (of course). Trying pfx.");
+
+                string pfxPath = Path.Combine(secretsFolderPath, builder.Configuration["App:CertBaseName"] + ".pfx");
+                certificate = new X509Certificate2(pfxPath, "");
+            }
 
             builder.WebHost.ConfigureKestrel(options =>
             {
